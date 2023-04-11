5faa3a995e8ba4deec118427471948cf19372533
Frank <sungwoo@treenod.com>
2023-04-11 15:18:24 +0900

    ✨ 현재 열려있는 페이지의 모든 데이터를 다운로드합니다.

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){var e=document.documentElement.innerHTML;var a=document.createElement('a');a.href='data:text/plain;charset=utf-8,'+encodeURIComponent(e);a.target='_blank';a.download='page.html';a.click();})();
